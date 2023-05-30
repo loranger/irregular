a26c172948a42b68383b53b281d6973808a4c819
Laurent Goussard <loranger@free.fr>
2023-05-30 17:29:00 +0200

    :lipstick: Nicer color and bolder fonts

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@ onMounted(() => {
 <template>
   <main class="grid min-h-screen place-items-center bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-      <p class="text-base font-semibold text-indigo-600 dark:text-indigo-400">Irregular verb</p>
+      <p class="text-base font-semibold text-blue-600 dark:text-blue-400">Irregular verb</p>
       <Roller 
         @click="state.expand = true"
         default-value="loading…" 
@@ -49,32 +49,32 @@ onMounted(() => {
         class="mt-4 text-4xl md:text-8xl cursor-pointer font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl justify-center"></Roller>
 
       <div 
-      @click="state.expand = !state.expand"
-        class="min-h-[15rem] flex flex-col justify-around">
+        @click="state.expand = !state.expand"
+        class="flex flex-col justify-around min-h-[15rem] cursor-pointer">
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">Past simple</p>
-            <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb['past-simple'])"></h3>
+            <p class="text-xs font-semibold text-blue-600 dark:text-blue-400">Past simple</p>
+            <h3 class="text-2xl dark:text-gray-100 font-semibold" v-text="capitalize(state.verb['past-simple'])"></h3>
           </div>
         </Transition>
 
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">Past participle</p>
-            <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb['past-participle'])"></h3>
+            <p class="text-xs font-semibold text-blue-600 dark:text-blue-400">Past participle</p>
+            <h3 class="text-2xl dark:text-gray-100 font-semibold" v-text="capitalize(state.verb['past-participle'])"></h3>
           </div>
         </Transition>
 
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">French 🇫🇷</p>
-            <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb.french)"></h3>
+            <p class="text-xs font-semibold text-blue-600 dark:text-blue-400">French 🇫🇷</p>
+            <h3 class="text-2xl dark:text-gray-100 font-semibold" v-text="capitalize(state.verb.french)"></h3>
           </div>
         </Transition>
       </div>
 
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="randomVerb()">Random verb</button>
+        <button type="button" class="rounded-md bg-blue-600 px-3 py-2 text-xl font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" @click="randomVerb()">Random verb</button>
       </div>
     </div>
   </main>
