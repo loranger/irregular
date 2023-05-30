58cfa0d63504ad85d09712205e5ebffeeb5c9773
Laurent Goussard <loranger@free.fr>
2023-05-30 17:18:09 +0200

    :lipstick: Wider font and click area on empty display

--- src/App.vue
+++ src/App.vue
@@ -46,33 +46,35 @@ onMounted(() => {
         :value="`To ${state.verb.base}`" 
         char-set="alphabet" 
         :duration="1000" 
-        class="mt-4 text-3xl cursor-pointer font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl justify-center"></Roller>
+        class="mt-4 text-4xl md:text-8xl cursor-pointer font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl justify-center"></Roller>
 
-      <div class="min-h-[15rem] flex flex-col justify-around">
+      <div 
+      @click="state.expand = !state.expand"
+        class="min-h-[15rem] flex flex-col justify-around">
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-base font-semibold text-indigo-600 dark:text-indigo-400">Past simple</p>
+            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">Past simple</p>
             <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb['past-simple'])"></h3>
           </div>
         </Transition>
 
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-base font-semibold text-indigo-600 dark:text-indigo-400">Past participle</p>
+            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">Past participle</p>
             <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb['past-participle'])"></h3>
           </div>
         </Transition>
 
         <Transition>
           <div class="" v-if="state.expand">
-            <p class="text-base font-semibold text-indigo-600 dark:text-indigo-400">French</p>
+            <p class="text-xs font-semibold text-indigo-600 dark:text-indigo-400">French 🇫🇷</p>
             <h3 class="text-2xl dark:text-gray-300" v-text="capitalize(state.verb.french)"></h3>
           </div>
         </Transition>
       </div>
 
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="randomVerb()">Random verb</button>
+        <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="randomVerb()">Random verb</button>
       </div>
     </div>
   </main>
